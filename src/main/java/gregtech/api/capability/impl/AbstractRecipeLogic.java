cff89c1986a15bc7eceafb4c2eb67c820ca0cdfc
Johny709 <janjoseph.merida@gmail.com>
2025-07-24 11:58:22 -0400

    fix void recipe excess

--- src/main/java/gregtech/api/capability/impl/AbstractRecipeLogic.java
+++ src/main/java/gregtech/api/capability/impl/AbstractRecipeLogic.java
@@ -299,8 +299,8 @@ public abstract class AbstractRecipeLogic extends MTETrait implements IWorkable 
         boolean ignoreOutputFluidSpace = this.metaTileEntity instanceof MultiblockWithDisplayBase && ((MultiblockWithDisplayBase) this.metaTileEntity).isFluidInfSink();
         return (totalEUt >= 0 ? getEnergyStored() >= (totalEUt > getEnergyCapacity() / 2 ? resultOverclock[0] : totalEUt) :
                 (getEnergyStored() - resultOverclock[0] <= getEnergyCapacity())) &&
-                ignoreOutputItemSpace || MetaTileEntity.addItemsToItemHandler(exportInventory, true, recipe.getAllItemOutputs(exportInventory.getSlots())) &&
-                ignoreOutputFluidSpace || MetaTileEntity.addFluidsToFluidHandler(exportFluids, true, recipe.getFluidOutputs()) &&
+                (ignoreOutputItemSpace || MetaTileEntity.addItemsToItemHandler(exportInventory, true, recipe.getAllItemOutputs(exportInventory.getSlots()))) &&
+                (ignoreOutputFluidSpace || MetaTileEntity.addFluidsToFluidHandler(exportFluids, true, recipe.getFluidOutputs())) &&
                 recipe.matches(true, importInventory, importFluids);
     }
 
