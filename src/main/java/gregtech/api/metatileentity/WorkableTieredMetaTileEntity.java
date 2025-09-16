ed5c1d1aa1320124574d00fa55f1c0749e4c4e2f
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-09-16 10:01:02 -0400

    add ghost circuit to singleblocks

--- src/main/java/gregtech/api/metatileentity/WorkableTieredMetaTileEntity.java
+++ src/main/java/gregtech/api/metatileentity/WorkableTieredMetaTileEntity.java
@@ -7,6 +7,7 @@ import codechicken.lib.vec.Matrix4;
 import gregtech.api.GTValues;
 import gregtech.api.capability.impl.FilteredFluidHandler;
 import gregtech.api.capability.impl.FluidTankList;
+import gregtech.api.capability.impl.ItemHandlerList;
 import gregtech.api.capability.impl.RecipeLogicEnergy;
 import gregtech.api.recipes.Recipe;
 import gregtech.api.recipes.RecipeMap;
@@ -26,6 +27,7 @@ import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -34,6 +36,8 @@ public abstract class WorkableTieredMetaTileEntity extends TieredMetaTileEntity 
 
     protected final RecipeLogicEnergy workable;
     protected final OrientedOverlayRenderer renderer;
+    protected ItemStackHandler ghostCircuitInventory = new ItemStackHandler(1);
+    protected IItemHandlerModifiable combinedInputInventory = new ItemHandlerList(Arrays.asList(this.ghostCircuitInventory, this.importItems));
 
     public WorkableTieredMetaTileEntity(ResourceLocation metaTileEntityId, RecipeMap<?> recipeMap, OrientedOverlayRenderer renderer, int tier) {
         this(metaTileEntityId, recipeMap, renderer, tier, 16);
@@ -52,7 +56,12 @@ public abstract class WorkableTieredMetaTileEntity extends TieredMetaTileEntity 
     }
 
     protected RecipeLogicEnergy createWorkable(RecipeMap<?> recipeMap, int recipeCacheSize) {
-        return new RecipeLogicEnergy(this, recipeMap, () -> energyContainer, recipeCacheSize);
+        return new RecipeLogicEnergy(this, recipeMap, () -> energyContainer, recipeCacheSize) {
+            @Override
+            protected IItemHandlerModifiable getInputInventory() {
+                return combinedInputInventory;
+            }
+        };
     }
 
     @Override
@@ -164,6 +173,7 @@ public abstract class WorkableTieredMetaTileEntity extends TieredMetaTileEntity 
     public NBTTagCompound writeToNBT(NBTTagCompound data) {
         NBTTagCompound tagCompound = super.writeToNBT(data);
         tagCompound.setBoolean("UseOptimizedRecipeLookUp", this.workable.getUseOptimizedRecipeLookUp());
+        tagCompound.setTag("GhostCircuit", this.ghostCircuitInventory.serializeNBT());
         return tagCompound;
     }
 
@@ -173,5 +183,7 @@ public abstract class WorkableTieredMetaTileEntity extends TieredMetaTileEntity 
         if (data.hasKey("UseOptimizedRecipeLookUp")) {
             this.workable.setUseOptimizedRecipeLookUp(data.getBoolean("UseOptimizedRecipeLookUp"));
         }
+        if (data.hasKey("GhostCircuit"))
+            this.ghostCircuitInventory.deserializeNBT(data.getCompoundTag("GhostCircuit"));
     }
 }
