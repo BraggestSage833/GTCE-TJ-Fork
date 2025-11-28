d3b9442121e11cffb244eb6f74b35520de6a6240
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-11-27 20:10:17 -0500

    fluid slot rework (#16)
    * add ghost circuit widget
    
    * add ghost circuit to singleblocks
    
    * add ghost circuit to input buses
    
    * fix item recipes not running with ghost circuit
    
    * rework fluid slot interaction
    
    * update fluid slot tooltip
    
    * fix container getting voided when inserting into full player inventory
    
    * add fluid slot interaction for super and quantum tanks

--- src/main/java/gregtech/api/util/GTUtility.java
+++ src/main/java/gregtech/api/util/GTUtility.java
@@ -21,6 +21,7 @@ import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
@@ -50,6 +51,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -748,5 +750,44 @@ public class GTUtility {
 
     public static int getRandomIntXSTR(int bound) {
         return random.nextInt(bound);
+    }
+
+    /**
+     * Tries to insert ItemStack into player inventory. Make sure to return a new copy of ItemStack before inserting an item from ItemHandler
+     * @param inventory player inventory
+     * @param stack the ItemStack to insert
+     * @param mainInventory if you should insert in main inventory
+     * @param armor if you should insert in armor slots
+     * @param offHand if you should insert in off-Hand slot
+     * @return ItemStack reminder. returns empty when ItemStack is fully inserted. returns the stack unmodified when unable to insert at all.
+     */
+    public static ItemStack insertInPlayerInventory(InventoryPlayer inventory, ItemStack stack, boolean mainInventory, boolean armor, boolean offHand) {
+        if (inventory == null || stack.isEmpty())
+            return stack;
+        if (mainInventory)
+            insertToAvailableSlots(inventory.mainInventory, stack);
+        if (armor)
+            insertToAvailableSlots(inventory.armorInventory, stack);
+        if (offHand)
+            insertToAvailableSlots(inventory.offHandInventory, stack);
+        return stack;
+    }
+
+    private static void insertToAvailableSlots(NonNullList<ItemStack> stackList, ItemStack stack) {
+        for (int i = 0; i < stackList.size(); i++) {
+            ItemStack inventoryStack = stackList.get(i);
+            if (inventoryStack.isEmpty()) {
+                int shrink = Math.min(stack.getCount(), 64);
+                ItemStack newStack = stack.copy();
+                newStack.setCount(shrink);
+                stackList.set(i, newStack);
+                stack.shrink(shrink);
+            } else if (inventoryStack.isItemEqual(stack) && ItemStack.areItemStackShareTagsEqual(inventoryStack, stack)) {
+                int reminder = inventoryStack.getMaxStackSize() - inventoryStack.getCount();
+                int shrink = Math.min(reminder, stack.getCount());
+                inventoryStack.grow(shrink);
+                stack.shrink(shrink);
+            }
+        }
     }
 }
